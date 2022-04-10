e9513a42faa2ab7f45018c2637928fe4f33ee307
samuel.gomez <contact@samuel.gomez.fr>
2022-04-11 01:13:53 +0200

    path(prod) fix

--- src/members/download.js
+++ src/members/download.js
@@ -13,13 +13,8 @@ const downloadDetails = (req, res) => {
       case 'error404':
         res.status(404).send(setResponseInvalid({ code: 404 }));
         break;
-      default:
-          console.log(process.env.NODE_ENV)
-        res.sendFile('details.csv', { root: process.env.NODE_ENV === 'development' ? 'dist' : '' });
-       /*  const pathFile = path.join(__dirname, 'details.csv');
-        console.log('pathFile', pathFile)
-        res.sendFile(pathFile); */
-        
+      default:         
+        res.sendFile('details.csv', { root: process.env.NODE_ENV === 'development' ? 'dist' : '.' });        
         break;
     }
   }, timeOut);
